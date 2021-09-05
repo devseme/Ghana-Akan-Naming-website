9be46bc492b986428d7cd69633753b0f601c7fe2
devseme <ian.ochenge@student.moringaschool.com>
2021-09-05 20:04:28 +0300

    modified function

--- script.js
+++ script.js
@@ -21,44 +21,52 @@ Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD 
  */
 
   function birthDay(){
-       let DD=parsefloat(getElementById("day").value);
-       let MM=parsefloat(getElementById("month").value);
-       let YY=parsefloat(getElementById("year").value);
+       let DD=parseInt(document.getElementById("day").value);
+       let MM=parseInt(document.getElementById("month").value);
+       let YY=parseInt(document.getElementById("year").value);
 
        let vv=DD=0;
        let CC=(YY -1)/100 +1;
        let weekDay =( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
        console.log(weekDay);
             
-         document.getElementById("result").innerHTML=math.Round(weekDay);
+         document.getElementById("result").textContent=Math.round(weekDay);
+         document.getElementById("akanName").textContent=answer;
          
          /* Array for names*/
          let maleNames=["Kwasi","Kwadwo","Kwabena","kwaku","Yaw","Kofi","Kwame"];
          let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
             
          let choice=document.getElementById("select").value;
+         
 
          /*conditional statement if*/
            if((choice==="male" &&(DD>0 && DD<31) && (MM>0 && MM<12) &&YY>=1)){
                   
-                console.log(maleNames(math.Round(weekDay)));
-                  document.getElementById("akanName").innerHTML=maleNames[math.Round(weekDay)];
+                console.log(maleNames(Math.round(weekDay)));
+                 //document.getElementById("akanName").textContent=maleNames[Math.round(weekDay)];
+                let answer=maleNames[Math.round(weekDay)];
 
+                document.getElementById("akanName").textContent=answer;
              }
 
             else if((choice==="female" &&(DD>0 && DD<31) && (MM>0 && MM<12) &&YY>=1))
                   {
-                console.log(femaleNames(math.Round(weekDay)));
+                console.log(femaleNames(Math.round(weekDay)));
                  
-                document.getElementById("akanName").innerHTML=femaleNames[math.Round(weekDay)]; 
+               // document.getElementById("akanName").textContent=femaleNames[Math.round(weekDay)]; 
+               // answer=femaleNames[Math.round(weekDay)];
+               document.getElementById("akanName").textContent=answer;
+              }
+             
             }
             /*in case of wrong input*/
-               else{
+               /*{
                    console.log("You entered invalid data")
 
                       alert("The data you entered is invalid.Try again!")
-                        document.getElementById("akanName").innerHTML="You entered invalid data";
+                        document.getElementById("akanName").textContent="You entered invalid data";
                        
-               }
+               }*/
 
-  }
+  
